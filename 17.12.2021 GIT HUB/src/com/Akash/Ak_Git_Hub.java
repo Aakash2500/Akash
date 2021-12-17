48fb6c664faf1b7cc5ecf5138e18eeb4557a0f6f
Aakash2500 <96283100+Aakash2500@users.noreply.github.com>
2021-12-17 20:27:24 +0530

    Update Ak_Git_Hub.java

--- 17.12.2021 GIT HUB/src/com/Akash/Ak_Git_Hub.java
+++ 17.12.2021 GIT HUB/src/com/Akash/Ak_Git_Hub.java
@@ -4,5 +4,8 @@ public class Ak_Git_Hub {
 
 	public static void main(String[] args) {
 		System.out.println("Hiii ....Helooo");
+		
+		System.out.println("Hiii ....Helooo");
+		System.out.println("Hiii ....Helooo");
 	}
 }
